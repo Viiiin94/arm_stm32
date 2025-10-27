059fdae2f3b5d23035a07b0a65ce77fd685c1677
Viiiin94 <mynameisyyb@naver.com>
2025-10-27 16:08:11 +0900

    i2c 통신 lcd, 스텝모터 기능추가

---
+++ 07_STEPPER/Core/Inc/stepper.h
@@ -0,0 +1,28 @@
+#ifndef INC_STEPPER_H_
+#define INC_STEPPER_H_
+
+#include "main.h"
+
+
+#define STEPS_PER_REVOLATION	4096
+#define DIR_CW			0	// 시계 방향
+#define DIR_CCW			1	// 반시계 방향
+
+#define IN1_PIN		GPIO_PIN_1
+#define IN1_GPIO_PORT	GPIOB
+#define IN2_PIN		GPIO_PIN_15
+#define IN2_GPIO_PORT	GPIOB
+#define IN3_PIN		GPIO_PIN_14
+#define IN3_GPIO_PORT	GPIOB
+#define IN4_PIN		GPIO_PIN_13
+#define IN4_GPIO_PORT	GPIOB
+
+
+
+
+void stepMotor(uint8_t step);
+void rotateSteps(uint16_t steps, uint8_t direction);
+void rotateDegrees(uint16_t degrees, uint8_t direction);
+
+
+#endif /* INC_STEPPER_H_ */
